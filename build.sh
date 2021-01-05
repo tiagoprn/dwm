24ad222596845de5e3ac1b9654ff4d2eac2c9483
tiago.paranhos <tiagoprn@gmail.com>
2021-01-05 18:14:00 -0300

    feat: organizing documentation.

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+echo 'Compiling and installing dwm...'
 DESTDIR=~/local PREFIX='' make clean install
+echo 'Converting man page to html...'
+groff -man -T html larbs.mom > larbs.html
+echo 'Finished'
